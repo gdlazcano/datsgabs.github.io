f8e9439fd1a8525b1fba9776679d5aaff66300d9
Gabriel Lazcano <gabriel.david.lazcano@gmail.com>
2021-03-01 20:39:42 -0600

    added custom TOC

---
+++ assets/js/toc.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const observerForTableOfContentActiveState = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                const id = entry.target.getAttribute("id")
+                if (entry.isIntersecting) {
+                    document
+                        .querySelector(`nav li a[href="#${id}"]`)
+                        .parentElement.classList.add("active")
+                } else {
+                    document
+                        .querySelector(`nav li a[href="#${id}"]`)
+                        .parentElement.classList.remove("active")
+                }
+            })
+        }
+    )
+    document
+        .querySelectorAll("h1[id],h2[id],h3[id]")
+        .forEach((section) => {
+            observerForTableOfContentActiveState.observe(section)
+        })
+    // Track all sections that have an `id` applied
+})
