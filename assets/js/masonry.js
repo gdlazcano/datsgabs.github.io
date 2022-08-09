b1f748274aacf0f6d43f80876e77a802e6ad55d7
Gabriel <gabriel.david.lazcano@gmail.com>
2022-08-09 15:47:39 -0500

    new scss

--- assets/js/masonry.js
+++ assets/js/masonry.js
@@ -1,43 +1,43 @@
 const masonryLayout = (containerElem, itemsElems, columns) => {
-    console.log(containerElem, itemsElems, columns)
+  console.log(containerElem, itemsElems, columns);
 
-    containerElem.classList.add("masonry-layout", `columns-${columns}`)
-    let columnsElements = []
+  containerElem.classList.add("masonry-layout", `columns-${columns}`);
+  let columnsElements = [];
 
-    for (let i = 1; i <= columns; i++) {
-        let column = document.createElement("ul")
-        column.classList.add("masonry-column", `column-${i}`)
-        containerElem.appendChild(column)
-        columnsElements.push(column)
+  for (let i = 1; i <= columns; i++) {
+    let column = document.createElement("ul");
+    column.classList.add("masonry-column", `column-${i}`);
+    containerElem.appendChild(column);
+    columnsElements.push(column);
+  }
+
+  for (let m = 0; m < Math.ceil(itemsElems.length / columns); m++) {
+    for (let n = 0; n < columns; n++) {
+      let item = itemsElems[m * columns + n];
+      if (item) {
+        columnsElements[n].appendChild(item);
+        item.classList.add("masonry-item");
+      }
     }
+  }
+};
 
-    for (let m = 0; m < Math.ceil(itemsElems.length / columns); m++) {
-        for (let n = 0; n < columns; n++) {
-            let item = itemsElems[m * columns + n]
-            if (item) {
-                columnsElements[n].appendChild(item)
-                item.classList.add("masonry-item")
-            }
-        }
-    }
+try {
+  masonryLayout(
+    document.querySelector(".list"),
+    document.querySelectorAll(".list-card"),
+    2
+  );
+} catch (error) {
+  console.log(error);
 }
 
 try {
-    masonryLayout(
-        document.querySelector(".listing"),
-        document.querySelectorAll(".list-card"),
-        2
-    )
+  masonryLayout(
+    document.querySelector(".replies"),
+    document.querySelectorAll(".reply"),
+    2
+  );
 } catch (error) {
-    console.log(error)
-}
-
-try {
-    masonryLayout(
-        document.querySelector(".replies"),
-        document.querySelectorAll(".reply"),
-        2
-    )
-} catch (error) {
-    console.log(error)
+  console.log(error);
 }
